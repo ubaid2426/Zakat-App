d1d2391893581de7dd1241b1aad61f333518f543
mac <fatalal@MacBookPro.lan>
2025-01-05 00:17:21 +0500

    ,,,

--- lib/Screens/All_Category/Individual/Screen/masjidConst/masjid_const.dart
+++ lib/Screens/All_Category/Individual/Screen/masjidConst/masjid_const.dart
@@ -1,152 +1,244 @@
-// import 'package:flutter/material.dart';
-// import 'package:sadqahzakat/Screens/donation_service.dart';
-// import 'package:sadqahzakat/components/donate.dart';
-// import 'package:sadqahzakat/model/doantion_model.dart';
-// // import 'package:zakat_app/services/donation_service.dart'; // Import the service
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_expandable_text/flutter_expandable_text.dart';
+// import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
-// class MasjidConst extends StatefulWidget {
-//   const MasjidConst({super.key});
+class MasjidConstForm extends StatefulWidget {
+  const MasjidConstForm({super.key});
 
-//   @override
-//   _MasjidConstState createState() => _MasjidConstState();
-// }
+  @override
+  // ignore: library_private_types_in_public_api
+  _MasjidConstFormState createState() => _MasjidConstFormState();
+}
 
-// class _MasjidConstState extends State<MasjidConst> {
-//   String selectedSort = 'Not finished projects first';
-//   List<DonationModel> sortedClothes = [];
+class _MasjidConstFormState extends State<MasjidConstForm> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _addressController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final List<Map<String, dynamic>> donationOptionsmeal = [
+    {'title': 'You will give land', 'price': 0},
+    {'title': 'You contribute in Constuction \n like (Bricks, Marble, cement, Electric Equipment etc)', 'price': 0},
+    // {'title': 'Electric WheelChair', 'price': 10000},
+  ];
+  Map<String, dynamic>? selectedDonation;
+  // String? _selectedDonationOption;
+  String? donationtitle;
+  Future<void> _submitForm() async {
+    if (_formKey.currentState!.validate()) {
+      // Gather data from form fields
+      final data = {
+        "name": _nameController.text,
+        "contact_number": _phoneController.text,
+        "address": _addressController.text,
+        // "donation_type": _selectedDonationOption,
+        "donationtitle": donationtitle,
+      };
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     _fetchSortedClothes();
-//   }
+      try {
+        print(data); // For debugging
+        // Send the POST request
+        final response = await http.post(
+          Uri.parse('YOUR_API_URL_HERE'), // Replace with your API endpoint
+          headers: {"Content-Type": "application/json"},
+          body: jsonEncode(data),
+        );
 
-//   Future<void> _fetchSortedClothes() async {
-//     String filterValue = 'unfinished'; // Default filter, change as needed
-//     switch (selectedSort) {
-//       case 'finished projects':
-//         filterValue = 'finished';
-//         break;
-//       case 'Not finished projects first':
-//         filterValue = 'unfinished';
-//         break;
-//     }
-//     String sortvalue = 'newest';
-//      switch (selectedSort) {
-//       case 'Oldest Items First':
-//         sortvalue = 'oldest'; // Keep the same filter for sorting
-//         break;
-//       case 'Newest Items First':
-//         sortvalue = 'newest'; // Keep the same filter for sorting
-//         break;
-//       case 'Sort by Remaining Value: Low to High':
-//         sortvalue = 'remaining_low_to_high'; // Keep the same filter for sorting
-//         break;
-//       case 'Sort by Remaining Value: High to Low':
-//         sortvalue = 'remaining_high_to_low'; // Keep the same filter for sorting
-//         break;
-//     }
-//     final donations = await DonationService().fetchDonations(
-//         sort: sortvalue, filter: filterValue, category: "masjidConst", selectCategory:"individual");
+        // Check the response status
+        if (response.statusCode == 201) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+                content: Text('Donation request submitted successfully')),
+          );
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Failed to submit: ${response.body}')),
+          );
+        }
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error: $e')),
+        );
+      }
+    }
+  }
 
-//     setState(() {
-//       sortedClothes = donations;
-//     });
-//   }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Widow Support Form'),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF33A248), Color(0xFFB2EA50)],
+              begin: Alignment.bottomRight,
+              end: Alignment.topLeft,
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const ExpandableText(
+                  "Save a life today – your blood can be someone's second chance at life!",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black87,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                _buildTextField(
+                  controller: _nameController,
+                  label: 'Name',
+                  hint: 'E.g. Ali',
+                  icon: Icons.person,
+                ),
+                const SizedBox(height: 16),
+                _buildTextField(
+                  controller: _phoneController,
+                  label: 'Contact Number',
+                  hint: 'E.g. +92 3000000000',
+                  icon: Icons.phone,
+                  keyboardType: TextInputType.phone,
+                ),
+                const SizedBox(height: 16),
+                // _buildDropdown(),
+                _buildDonationDropdown(),
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         flexibleSpace: Container(
-//           decoration: const BoxDecoration(
-//             gradient: LinearGradient(
-//               colors: [
-//                 Color(0xFF33A248),
-//                 Color(0xFFB2EA50),
-//               ],
-//               begin: Alignment.topRight,
-//               end: Alignment.topLeft,
-//             ),
-//           ),
-//         ),
-//         leading: IconButton(
-//           onPressed: () => Navigator.pop(context),
-//           icon: const Icon(Icons.arrow_back),
-//         ),
-//         title: const Text("Masjid Construction"),
-//         actions: [
-//           PopupMenuButton<String>(
-//             onSelected: (value) {
-//               setState(() {
-//                 selectedSort = value;
-//                 _fetchSortedClothes();
-//               });
-//             },
-//             itemBuilder: (BuildContext context) => [
-//               const PopupMenuItem(
-//                 value: 'finished projects',
-//                 child: Text('Finished Projects'),
-//               ),
-//               const PopupMenuItem(
-//                 value: 'Not finished projects first',
-//                 child: Text('Not Finished Projects First'),
-//               ),
-//               const PopupMenuItem(
-//                 value: 'Oldest Items First',
-//                 child: Text('Oldest Items First'),
-//               ),
-//               const PopupMenuItem(
-//                 value: 'Newest Items First',
-//                 child: Text('Newest Items First'),
-//               ),
-//               const PopupMenuItem(
-//                 value: 'Sort by Remaining Value: Low to High',
-//                 child: Text('Sort by Remaining Value: Low to High'),
-//               ),
-//               const PopupMenuItem(
-//                 value: 'Sort by Remaining Value: High to Low',
-//                 child: Text('Sort by Remaining Value: High to Low'),
-//               ),
-//             ],
-//             icon: const Icon(Icons.sort),
-//           ),
-//         ],
-//       ),
-//       backgroundColor: Colors.white,
-//       body: SafeArea(
-//         child: SingleChildScrollView(
-//           child: Column(
-//             children: [
-//               if (sortedClothes.isNotEmpty)
-//                 Column(
-//                   children: sortedClothes.map((donation) {
-//                     return Data(
-//                       imageUrl: donation.imageUrl,
-//                       title: donation.title,
-//                       description: donation.description,
-//                       projectvalue: donation.projectValue,
-//                       paidvlaue: donation.paidValue, selectcategory: '',
-//                     );
-//                   }).toList(),
-//                 )
-//               else
-//                 const Padding(
-//                   padding: EdgeInsets.all(20.0),
-//                   child: Text(
-//                     "This domain does not need donations at the moment.",
-//                     style: TextStyle(
-//                       fontSize: 18,
-//                       fontWeight: FontWeight.bold,
-//                       color: Colors.red,
-//                     ),
-//                     textAlign: TextAlign.center,
-//                   ),
-//                 ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+                const SizedBox(height: 20),
+                _buildTextField(
+                  controller: _addressController,
+                  label: 'Current Address',
+                  hint: 'E.g House no, Society and City',
+                  icon: Icons.home_filled,
+                  maxLines: 3,
+                ),
+                const SizedBox(height: 20),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 30),
+                    child: ElevatedButton.icon(
+                      onPressed: _submitForm,
+                      icon: const Icon(Icons.send),
+                      label: const Text('SUBMIT'),
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 30, vertical: 15),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        backgroundColor: const Color(0xFF7fc23a),
+                        textStyle: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String label,
+    required String hint,
+    required IconData icon,
+    TextInputType keyboardType = TextInputType.text,
+    int maxLines = 1,
+  }) {
+    return TextFormField(
+      controller: controller,
+      keyboardType: keyboardType,
+      maxLines: maxLines,
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hint,
+        prefixIcon: Icon(icon, color: const Color(0xFF7fc23a)),
+        filled: true,
+        fillColor: Colors.grey[100],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: Color(0xFF7fc23a), width: 2),
+        ),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter your $label';
+        }
+        return null;
+      },
+    );
+  }
+
+
+  Widget _buildDonationDropdown() {
+    return Material(
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          children: [
+            const Text(
+              'Select a donation option:',
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              decoration: BoxDecoration(
+                color: const Color(0xFF7fc23a),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: DropdownButton<Map<String, dynamic>>(
+                isExpanded: true,
+                hint: const Text(
+                  'Choose a donation option',
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                ),
+                value: selectedDonation,
+                items: donationOptionsmeal
+                    .map((option) => DropdownMenuItem<Map<String, dynamic>>(
+                          value: option,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(option['title'], style: TextStyle(fontSize: 13),),
+                              Text('Rs ~ ${option['price']}'),
+                            ],
+                          ),
+                        ))
+                    .toList(),
+                onChanged: (newValue) {
+                  setState(() {
+                    selectedDonation = newValue;
+                    donationtitle = selectedDonation?['title'];
+                    // updateTotalAmount();
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
